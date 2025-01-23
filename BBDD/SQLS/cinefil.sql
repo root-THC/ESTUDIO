95cc2c41e74e71109d4e2a96f0b6900e9f5ad117
root-THC <riicardomoreenoi@gmail.com>
2025-01-23 12:57:02 +0100

    PUSH SQL

--- BBDD/SQLS/cinefil.sql
+++ BBDD/SQLS/cinefil.sql
@@ -74,7 +74,7 @@ CREATE TABLE ciutatxcompanyia (
     IdCiutat SMALLINT,
     IdCompanyia SMALLINT,
     CONSTRAINT id_ciutatxcompanyia_pk PRIMARY KEY (IdCiutat,IdCompanyia)
-    CONSTRAINT idciutat_ciutatxcompanyia
+    CONSTRAINT ciutatxcompanyia_
 
 
 );
