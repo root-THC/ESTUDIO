6083d1e908ddb57a047b54ab354bfb4e416badf3
root-THC <riicardomoreenoi@gmail.com>
2025-01-23 12:50:32 +0100

    PUSH SQL

--- BBDD/SQLS/cinefil.sql
+++ BBDD/SQLS/cinefil.sql
@@ -62,7 +62,7 @@ CREATE TABLE actorxtema (
 CREATE TABLE companyia (
     IdCompanyia SMALLINT,
     NomCompanyia VARCHAR(40),   
-    CONSTRAINT IdCompanyia PRIM
+    CONSTRAINT IdCompanyia_pk PRIMARY KEY
 
 
 
