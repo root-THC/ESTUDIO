f66fd7fe613bc61476c02163a47d384a3bcfcc5c
root-THC <riicardomoreenoi@gmail.com>
2025-01-27 09:33:05 +0100

    PUSH php

---
+++ IAW/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>PRUEBAS PHP</title>
+</head>
+<body>
+<!-- contenido  -->
+    <h1><?php echo "Hola desde PHP"; ?></h1>
+    <ul>
+
+    <?php
+    $maxTaules = 10;
+    $maxDinsLaTaula = 10;
+    for ($taulaDel = 1; $taulaDel <= $maxTaules; $taulaDel++) {
+        echo "<li>Taula del $taulaDel</li>";
+
+
+
+        for ($i=1; $1 <= $maxDinsLaTaula; i++) {
+
+            $resultat = $i * $taulaDel;
+        }
+
+    }
+
+
+
+
+
+
+    ?>
+    </ul>
+
+
+
+
+
+</body>
+</html>
