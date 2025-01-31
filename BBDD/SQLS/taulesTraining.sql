6ef6e8c870dac3007272d3fe755c905ba406ded1
root-THC <riicardomoreenoi@gmail.com>
2025-01-31 12:11:25 +0100

    sql-push

--- BBDD/SQLS/taulesTraining.sql
+++ BBDD/SQLS/taulesTraining.sql
@@ -225,4 +225,22 @@ SELECT fabcod, prodcod, descrip, exist FROM producto WHERE exist BETWEEN 25 AND 
 
 
 
+/*
+2. Obtenir els codis dels representants que han pres alguna comanda (evitant-ne la repetició).
 
+3. Obtenir les dades de les comandes realitzades pel client el codi és el 2111.
+
+4. Obtenir les dades de les comandes realitzades pel client el codi és el 2111 i que han estat presos pel representant el codi és el 103.
+
+5. Obtenir les dades de les comandes realitzades pel client el codi és el 2111, que han estat presos pel representant el codi del qual és el 103 i que sol·liciten articles del fabricant el codi del qual és ACI.
+
+6. Obtenir una llista de totes les comandes ordenades per client i, per cada client, ordenats per la data de la comanda (ascendentment)
+
+7. Obtenir les dades dels representants que pertanyen a loficina de codi 12 i 13 (cada representant només pertany a una oficina).
+
+8. Obtenir les dades de productes dels quals no hi ha existències o bé aquestes són desconegudes.
+
+9. Mostrar els representants que van ser contractats el 2003 (sumem 5000 a la data de contracte)
+
+10. Mostrar el nom i els dies que porta contractats els representants
+/*
