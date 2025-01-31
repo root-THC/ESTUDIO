9f4e85966473e798bdaa24c17fbf2acc937f2a8b
root-THC <riicardomoreenoi@gmail.com>
2025-01-31 12:54:51 +0100

    sql-push

--- BBDD/SQLS/taulesTraining.sql
+++ BBDD/SQLS/taulesTraining.sql
@@ -245,7 +245,7 @@ ALTER TABLE ONLY REPVENTA ADD CONSTRAINT FK_REPVENTA_JEFE FOREIGN KEY(JEFE) REFE
 
 /*
 7. Obtenir les dades dels representants que pertanyen a loficina de codi 12 i 13 (cada representant només pertany a una oficina).
-
+/*
 8. Obtenir les dades de productes dels quals no hi ha existències o bé aquestes són desconegudes.
 
 9. Mostrar els representants que van ser contractats el 2003 (sumem 5000 a la data de contracte)
