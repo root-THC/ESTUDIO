ecfd122914d323e13e1cfcf8ed932953248dafe6
root-THC <riicardomoreenoi@gmail.com>
2025-01-31 12:25:17 +0100

    sql-push

--- BBDD/SQLS/taulesTraining.sql
+++ BBDD/SQLS/taulesTraining.sql
@@ -226,7 +226,7 @@ SELECT fabcod, prodcod, descrip, exist FROM producto WHERE exist BETWEEN 25 AND 
 
 --3. Obtenir les dades de les comandes realitzades pel client el codi és el 2111.
 
-SELECT pednum,fecha,cliecod,repcod,fabcod,prodcod,cant,importe FROM cliente WHERE cliecod = '2111';
+SELECT pednum,fecha,cliecod,repcod,fabcod,prodcod,cant,importe FROM clien WHERE cliecod = '2111';
 
 
 
