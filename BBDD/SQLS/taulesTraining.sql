9c70d5a3977e54252620784316677509082a46f3
root-THC <riicardomoreenoi@gmail.com>
2025-01-31 12:03:47 +0100

    sql-push

--- BBDD/SQLS/taulesTraining.sql
+++ BBDD/SQLS/taulesTraining.sql
@@ -222,9 +222,9 @@ A. Consultes simples
 
 3. Obtenir les dades de les comandes realitzades pel client el codi és el 2111.
 
---4. Obtenir les dades de les comandes realitzades pel client el codi és el 2111 i que han estat presos pel representant el codi és el 103.
+4. Obtenir les dades de les comandes realitzades pel client el codi és el 2111 i que han estat presos pel representant el codi és el 103.
 
---5. Obtenir les dades de les comandes realitzades pel client el codi és el 2111, que han estat presos pel representant el codi del qual és el 103 i que sol·liciten articles del fabricant el codi del qual és ACI.
+-5. Obtenir les dades de les comandes realitzades pel client el codi és el 2111, que han estat presos pel representant el codi del qual és el 103 i que sol·liciten articles del fabricant el codi del qual és ACI.
 
 --6. Obtenir una llista de totes les comandes ordenades per client i, per cada client, ordenats per la data de la comanda (ascendentment)
 
