7e5a7b2fc2cf153be3d1f6f35e3303b9ab226af3
root-THC <riicardomoreenoi@gmail.com>
2025-01-31 11:30:51 +0100

    sql-push

--- BBDD/SQLS/restriccionesscott.sql
+++ BBDD/SQLS/restriccionesscott.sql
@@ -112,5 +112,5 @@ SELECT empno, ename, sal*12 "salari anual" FROM emp ORDER BY "salari anual"; -- 
 
 -- Mostrar nom del empleat el salari, el salari incrementat 20% , de qui mes cobra a menys
 
-SELECT empno, sal, sal*1.20 "salari +20%" FROM emp ORDER BY "salari "; 
+SELECT empno, sal, sal*1.20 "salari +20%" FROM emp ORDER BY "salari +20%"; 
 
