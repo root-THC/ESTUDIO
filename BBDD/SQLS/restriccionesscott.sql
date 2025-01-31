4627b4ffc2a37f1bc2f2391f24022f2da19790f5
root-THC <riicardomoreenoi@gmail.com>
2025-01-31 11:42:02 +0100

    sql-push

--- BBDD/SQLS/restriccionesscott.sql
+++ BBDD/SQLS/restriccionesscott.sql
@@ -114,5 +114,5 @@ ALTER TABLE  emp ADD CONSTRAINT emp_mgr_fk FOREIGN KEY (mgr) REFERENCES emp;
 
 --(
 -- Mostrar nom del empleat el salari, el salari incrementat 20% , de qui mes cobra a menys los que tengan el mismo salrio ordena por nombre
-SELECT ename, sal, sal*1.20 "salari +20%" FROM emp ORDER BY 3 DESC, ename DESC; -- 3 A que campos 
+SELECT ename, sal, sal*1.20 "salari +20%" FROM emp ORDER BY 3 DESC, ename DESC; -- 3 A que campo afecta por ej
 --)
