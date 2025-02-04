cb74f4e3bffe5252af70f28c529d230eb28a1f7d
root-THC <riicardomoreenoi@gmail.com>
2025-02-04 11:47:23 +0100

    sql-push

--- BBDD/SQLS/extranining.sql
+++ BBDD/SQLS/extranining.sql
@@ -40,7 +40,7 @@
 
 --9. Mostrar els representants que van ser contractats el 2003 (sumem 5000 a la data de contracte)
 
-SELECT *, fcontrato + 5000 AS "fecha contrato +" FROM repventa WHERE fcontrato;
+SELECT * FROM repventa WHERE fcontrato;
 
 --10. Mostrar el nom i els dies que porta contractats els representants
 
