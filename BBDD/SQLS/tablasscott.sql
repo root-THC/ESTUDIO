f90374232c2a0d746e5b637d82527d345fa8ba09
root-THC <riicardomoreenoi@gmail.com>
2025-02-05 12:41:15 +0100

    sql-push

--- BBDD/SQLS/tablasscott.sql
+++ BBDD/SQLS/tablasscott.sql
@@ -110,4 +110,4 @@ ALTER TABLE  emp ADD CONSTRAINT emp_mgr_fk FOREIGN KEY (mgr) REFERENCES emp;
 
 --Mostrar el nombre y numero de dias que lleva contratado un empleado.
 
-SELECT nombre , (CURRENT_DATE - fcontrato) AS "dias contratado" FROM repventa; 
+SELECT nombre , (CURRENT_DATE - fcontrat) AS "dias contratado" FROM repventa; 
