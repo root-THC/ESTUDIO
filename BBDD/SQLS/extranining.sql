b9573e04adfeb35125b9f287191ecdbc6171b76e
root-THC <riicardomoreenoi@gmail.com>
2025-02-05 13:50:39 +0100

    sql-push

--- BBDD/SQLS/extranining.sql
+++ BBDD/SQLS/extranining.sql
@@ -64,4 +64,4 @@ FROM repventa r JOIN oficina o ON r.ofinum = o.ofinum;
 --2. Obtenir els codis dels representants que han pres alguna comanda (evitant-ne la repetició).
 
 SELECT DISTINCT * , repcod
-FROM pedido p JOIN repcod r ON p.repcod = r
+FROM pedido p JOIN repcod r ON p.repcod = r.
