d4cda2e3f7102796cded74a1481916dc3a3f9b0b
root-THC <riicardomoreenoi@gmail.com>
2025-02-05 12:45:14 +0100

    sql-push

--- BBDD/SQLS/tablasscott.sql
+++ BBDD/SQLS/tablasscott.sql
@@ -110,4 +110,4 @@ ALTER TABLE  emp ADD CONSTRAINT emp_mgr_fk FOREIGN KEY (mgr) REFERENCES emp;
 
 --Mostrar el nombre y numero de dias que lleva contratado un empleado.
 
-SELECT *, (CURRENT_DATE - hiredate)/365. AS "años contratado" FROM emp; 
+SELECT *, (CURRENT_DATE - hiredate)/365.0 AS "años contratado" FROM emp; 
