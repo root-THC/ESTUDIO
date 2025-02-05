52a1968321f872f0d5a0411a13addfa8cb768749
root-THC <riicardomoreenoi@gmail.com>
2025-02-05 13:39:18 +0100

    sql-push

--- BBDD/SQLS/extranining.sql
+++ BBDD/SQLS/extranining.sql
@@ -56,5 +56,6 @@
 
 --B CONSULTA MULTITAULA
 
+1. Obtenir les dades dels productes les existències dels quals estiguin entre 25 i 40 unitats.
 SELECT nombre, ciudad, region
 FROM repventa JOIN oficina ON repventa.ofinum = oficina.ofinum;
