89e7535fb8f3703dc31d6baab5a06773137fa50c
root-THC <riicardomoreenoi@gmail.com>
2025-02-05 13:51:24 +0100

    sql-push

--- BBDD/SQLS/extranining.sql
+++ BBDD/SQLS/extranining.sql
@@ -63,5 +63,5 @@
 
 --2. Obtenir els codis dels representants que han pres alguna comanda (evitant-ne la repetició).
 
-SELECT DISTINCT * , repcod
+SELECT DISTINCT * , rep
 FROM pedido p JOIN repcod r ON p.repcod = r.repcod;
