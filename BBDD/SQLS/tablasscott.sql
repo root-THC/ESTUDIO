51dfdf49fa994f35d1fe028abdefee9f16982720
root-THC <riicardomoreenoi@gmail.com>
2025-02-05 12:35:41 +0100

    sql-push

--- BBDD/SQLS/tablasscott.sql
+++ BBDD/SQLS/tablasscott.sql
@@ -104,5 +104,5 @@ SELECT ename FROM emp WHERE LOWER(ename) LIKE '_l%'; -- MOSTRAR LOS QUE DE SEGUN
 --SELECT ename,sal, COALESCE(comm::TEXT,'-') comm,  -- el comm despues de los parentesis es la etiqueta que quiero que aparezca
 --sal+COALESCE(comm,0) 
 --"sal+comm"
-FROM emp;
+--FROM emp;
  -- MOSTRAR NOMBRE SALARIO Y COMISION COALESCE para que los valores NULL sean = 0 
