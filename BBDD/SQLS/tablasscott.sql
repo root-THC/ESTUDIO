a02c0fc8edf8e588907c2ee3a53886a5654992e4
root-THC <riicardomoreenoi@gmail.com>
2025-02-05 13:12:01 +0100

    sql-push

--- BBDD/SQLS/tablasscott.sql
+++ BBDD/SQLS/tablasscott.sql
@@ -115,3 +115,5 @@ ALTER TABLE  emp ADD CONSTRAINT emp_mgr_fk FOREIGN KEY (mgr) REFERENCES emp;
 
 --SELECT CURRENT_DATE - interval '1 hours 30 minutes';
 --SELECT CURRENT_TIMESTAMP - interval '1 hours 30 minutes';
+
+
