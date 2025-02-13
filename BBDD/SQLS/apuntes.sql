2a692c93a9b3c71a3ffafde484eeabba48310212
root-THC <riicardomoreenoi@gmail.com>
2025-02-13 12:17:59 +0100

    PUSH SQL

---
+++ BBDD/SQLS/apuntes.sql
@@ -0,0 +1,23 @@
+
+--APUNTES SQL
+
+
+
+--SELECTS :
+SELECT UPPER (‘hOlA’);
+resultado todo en mayúscula
+
+SELECT LOWER (‘hOlA’);
+resultado todo en minúscula
+
+
+--Fecha
+TO_DATE(‘12/07/24’,’dd/mm/yy’)
+
+--SECUENCIA
+
+CREATE SEQUENCE nombre_secuencia
+START WITH 10 INCREMENT BY 20;
+
+SELECT NEXTVAL ('nombre_secuencia')
+
