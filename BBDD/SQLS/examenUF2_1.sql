ea077371c032a29a612336d7cdbbbad130596af2
root-THC <riicardomoreenoi@gmail.com>
2025-02-14 11:45:27 +0100

    sql-push

--- BBDD/SQLS/examenUF2_1.sql
+++ BBDD/SQLS/examenUF2_1.sql
@@ -47,4 +47,7 @@ INSERT INTO usuari VALUES ('42234565H','656764234+32','a@gmail.com','A',TO_DATE(
 INSERT INTO usuari VALUES ('45155090F','656764234+32','x@gmail.com','B',TO_DATE('06-12-2000', 'DD-MM-YYYY'),NEXTVAL('numSoci_seq'),'Barcelona');
 
 DELETE FROM usuari WHERE lower(ciutat) = 'barcelona' AND (tipus = 'A' OR tipus = 'C') ;
+
+
+UPDATE SET cuota = 
 --DELETE FROM Usuari WHERE tipus = 'A'
