775405a87b1af386f89793dddc208501d159dbb2
root-THC <riicardomoreenoi@gmail.com>
2025-02-18 13:16:39 +0100

    sql-push

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -1,3 +1,4 @@
+*/
 8. Obtenir una llista de les comandes amb imports superiors a 150 euros, 
 mostrant el codi de la comanda, l'import, el nom del
 client que ho va sol·licitar, el nom del representant que va contactar
