4ad5bb8ecf30b43be2127a495cb47dfefad106ce
root-THC <riicardomoreenoi@gmail.com>
2025-02-18 13:54:32 +0100

    sql-push

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -12,7 +12,7 @@ SELECT pednum,
        importe, 
        c.nombre AS "Nombre Cliente", 
        p.fecha, 
-       r.nombre AS "Representante que contacto por primera vez"
+       r.nombre AS "Representante que contacto por primera vez",
 FROM pedido p 
 JOIN cliente c ON p.cliecod = c.cliecod
 JOIN repventa r ON c.repcod = r.repcod 
