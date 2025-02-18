3e354cae8fbdbc9fd19f9414de52593788f9269f
root-THC <riicardomoreenoi@gmail.com>
2025-02-18 13:16:29 +0100

    sql-push

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -1,1 +1,36 @@
+8. Obtenir una llista de les comandes amb imports superiors a 150 euros, 
+mostrant el codi de la comanda, l'import, el nom del
+client que ho va sol·licitar, el nom del representant que va contactar
+amb ell per primera vegada i la ciutat de loficina on el
+representant treballa.
+*/
+/*
+SELECT p.pednum , p.importe , c.nombre , r.nombre 
+FROM pedido p 
+JOIN 
+
+
+
+WHERE p.importe > 150;
+*/
+
+
+
+/*
+9. Llista les comandes preses durant el mes d'octubre de l'any 2003 ,
+mostrant només el número de la comanda, el seu import, el nom del
+client que ho va realitzar, la data i la descripció del producte
+sol·licitat
+
+
+
+10. Obtenir una llista de totes les comandes preses per representants de
+oficines de la regió Est, mostrant només el número del
+comanda, la descripció del producte i el nom del representant
+que ho va prendre
+
+11. Obtenir les comandes preses els mateixos dies en què un nou
+representant va ser contractat. Mostrar número de comanda, import,
+data comanda.
+*/
 
