01e9c90eaab2b5af5aff3c1d4a35eca4ce5f2c2d
root-THC <riicardomoreenoi@gmail.com>
2025-02-18 13:44:34 +0100

    sql-push

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -10,7 +10,7 @@ amb el client per primera vegada.
 
 SELECT pednum , importe , c.nombre AS "Nombre Cliente", p.fecha , r.nombre AS "Nombre Representante"
 FROM pedido p 
-JOIN repventa r ON c.repcod = r.repcod 
+
 JOIN cliente c ON p.cliecod = c.cliecod
 WHERE p.importe > 2000;
 
