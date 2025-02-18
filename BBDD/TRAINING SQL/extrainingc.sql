e338556486bfc1f2c21d874e84957d4d602f7634
root-THC <riicardomoreenoi@gmail.com>
2025-02-18 13:57:11 +0100

    sql-push

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -36,7 +36,7 @@ SELECT pednum,
 FROM pedido p 
 JOIN cliente c ON p.cliecod = c.cliecod
 JOIN repventa r ON c.repcod = r.repcod
-JOIN oficina 
+JOIN oficina o ON r.ofinum 
 WHERE p.importe > 2000;
 
 
