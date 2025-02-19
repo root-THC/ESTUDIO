db4a42d007eb6c06fc5e5db5474a6c747245b33a
root-THC <riicardomoreenoi@gmail.com>
2025-02-19 13:48:59 +0100

    sql-push

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -57,7 +57,7 @@ sol·licitat
 SELECT pednum , importe , fecha ,c.nombre , p.descrip
 FROM pedido p
 JOIN cliente c ON p.cliecod = c.cliecod;
-JOIN producto pr ON p.fabcod = pr.fabcod AND p.
+JOIN producto pr ON p.fabcod = pr.fabcod AND p.prodcod = 
 
 /*
 
