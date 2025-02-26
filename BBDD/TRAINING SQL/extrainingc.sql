e56e6b80f4f4ca898df8391ad74d88eabde87869
root-THC <riicardomoreenoi@gmail.com>
2025-02-26 12:46:38 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -92,7 +92,7 @@ ORDER BY 1;
 13. Mostra el nom, les vendes i la ciutat de l'oficina de cada representant de lempresa.
 */
 
--- PRIMERO VEMOS CUANTOS REPRESENTANTES DEBERIAN SALI CON UN sleect* from repventa; ya que nos dice de cada representante
+-- PRIMERO VEMOS CUANTOS REPRESENTANTES DEBERIAN SALI CON UN select* from repventa; ya que nos dice de cada representante
 
 
 
