ec213a6b86a849a1a830033b30209e3a693fd13f
root-THC <riicardomoreenoi@gmail.com>
2025-02-26 12:24:38 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -81,7 +81,7 @@ data comanda.
 16. Llista els noms dels representants que tenen una quota superior a la del director.
 
 
-Obtenir una llista dels representants que treballen en una oficina diferent de l'oficina on treballa el seu director, mostrant també el nom del director i el codi de l'oficina on treballa cadascun.
+17. Obtenir una llista dels representants que treballen en una oficina diferent de l'oficina on treballa el seu director, mostrant també el nom del director i el codi de l'oficina on treballa cadascun.
 
 El mateix exercici anterior, però en comptes d'ofinum, la ciutat.
 
