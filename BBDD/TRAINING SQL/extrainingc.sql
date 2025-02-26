99b623a9842cb20ca16ddb0e69b45f6b06916d05
root-THC <riicardomoreenoi@gmail.com>
2025-02-26 12:45:41 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -93,6 +93,9 @@ ORDER BY 1;
 */
 
 
+
+
+
 /*
 14. Obtenir una llista de la descripció dels productes per als quals existeix alguna comanda en què se sol·licita una quantitat més gran a les existències del producte.
 
