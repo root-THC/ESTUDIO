8c5a7f282901a719f12bd401589afaf5f946a1d4
root-THC <riicardomoreenoi@gmail.com>
2025-02-26 12:24:03 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -74,9 +74,9 @@ data comanda.
 
 12Obtenir les comandes preses els mateixos dies en què un nou representant va ser contractat. Mostrar número de comanda, import, data comanda.
 
-13Obtenir una llista amb parelles de representants i oficines on la quota del representant és més gran o igual que l'objectiu de la oficina, sigui o no l'oficina on treballa. Mostra Nom del representant, quota del mateix, Ciutat de l'oficina, objectiu de la mateixa.
+12Obtenir una llista amb parelles de representants i oficines on la quota del representant és més gran o igual que l'objectiu de la oficina, sigui o no l'oficina on treballa. Mostra Nom del representant, quota del mateix, Ciutat de l'oficina, objectiu de la mateixa.
 
-14Mostra el nom, les vendes i la ciutat de l'oficina de cada representant de lempresa.
+13Mostra el nom, les vendes i la ciutat de l'oficina de cada representant de lempresa.
 
 14Obtenir una llista de la descripció dels productes per als quals existeix alguna comanda en què se sol·licita una quantitat més gran a les existències del producte.
 
