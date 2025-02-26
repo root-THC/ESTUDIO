8198bae545e6ba8d80745acb65c4704f3a468584
root-THC <riicardomoreenoi@gmail.com>
2025-02-26 13:20:07 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -99,7 +99,7 @@ FROM repventa r LEFT join oficina o on r.ofinum = o.ofinum;
 */
 /*
 14. Obtenir una llista de la descripció dels productes per als quals existeix alguna comanda en què se sol·licita una quantitat més gran a les existències del producte.
-*/
+
 SELECT descrip , p.cant , pr.exist
 FROM producto pr
 JOIN pedido p ON p.fabcod || p.prodcod = pr.fabcod || pr.prodcod
