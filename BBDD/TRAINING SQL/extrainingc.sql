266daea0e2352c3ecb42fedbd4f03f28f401b333
root-THC <riicardomoreenoi@gmail.com>
2025-02-28 11:54:52 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -160,7 +160,7 @@ ON p.cliecod = c.cliecod;
 */
 
 
--- Lista de pedidos si hubiera algun repventa 
+-- Lista de pedidos si hubiera algun representante 
 SELECT  pednum "Num Pedido " , 
 p.importe "Importe Pedido", 
 r.nombre , 
