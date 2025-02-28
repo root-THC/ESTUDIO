3fbf863fefa64abc93d5eeedb5128ac7a6c430c1
root-THC <riicardomoreenoi@gmail.com>
2025-02-28 10:28:37 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -143,7 +143,7 @@ ON r.jefe = jefe.repcod;
 
 --Por cada representante mostrar el nombre i la ciutat de la seva oficina mas numero de comanda y importe de comanda
 
-SELECT r.nombre , COALESCE(o.ciudad::TEXT,  " sin oficina "  )  Oficina , p.pednum "Numero Pedido" , p.importe "Importe Pedido"
+SELECT r.nombre , COALESCE(o.ciudad::TEXT,  ' sin oficina "  )  Oficina , p.pednum "Numero Pedido" , p.importe "Importe Pedido"
 FROM repventa r 
 LEFT JOIN oficina o
 ON r.ofinum = o.ofinum
