07bb370a4581e46864d1f33b3d8502b4011b2a03
root-THC <riicardomoreenoi@gmail.com>
2025-02-28 11:52:19 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -165,7 +165,7 @@ r.nombre ,
 COALESCE(o.ciudad::TEXT,  'sin oficina'  )  Oficina , 
 pr.descrip "Descripción" , c.nombre
 FROM pedido p JOIN producto pr
-       ON p.fabcod = pr.fabcod AND p.prodcod =pr.prodcod
+              ON p.fabcod = pr.fabcod AND p.prodcod =pr.prodcod
        JOIN cliente c
        ON
 
