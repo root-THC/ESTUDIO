bbb0dc216834a8a4d68c5111694853f411835931
root-THC <riicardomoreenoi@gmail.com>
2025-02-28 10:18:17 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/extrainingc.sql
+++ BBDD/TRAINING SQL/extrainingc.sql
@@ -143,7 +143,7 @@ ON r.jefe = jefe.repcod;
 
 --Por cada representante mostrar el nombre i la ciutat de la seva oficina
 
-SELECT r.nombre , ciudad 
+SELECT nombre , ciudad 
 FROM repventa r LEFT JOIN oficina o
 ON r.ofinum = o.ofinum;
 
