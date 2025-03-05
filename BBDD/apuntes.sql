13ee47f1e092c77f420c9e1e99523fae5664b150
root-THC <riicardomoreenoi@gmail.com>
2025-03-05 13:11:25 +0100

    PUSH SQL

--- BBDD/apuntes.sql
+++ BBDD/apuntes.sql
@@ -21,7 +21,7 @@ TO_DATE(‘12/07/24’,’dd/mm/yy’)
 --SECUENCIA
 
 CREATE SEQUENCE nombre_secuencia
-START WITH 10 INCREMENT BY 20;
+START WITH 1 INCREMENT BY 1;
 
 SELECT NEXTVAL ('nombre_secuencia',otrovalor,otrovalor);
 
