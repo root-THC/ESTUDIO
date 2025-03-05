371867c4d6e59ce25966d77f4ba2cbceb179c837
root-THC <riicardomoreenoi@gmail.com>
2025-03-05 13:50:27 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/ejercicioscount.sql
+++ BBDD/TRAINING SQL/ejercicioscount.sql
@@ -32,4 +32,5 @@ GROUP by job;
 
 --Seguint el que s'ha explicat en l'ex. 27, Mostreu per cada departament, el nombre d'empleats que tenen comissió, la suma i la mitjana.
 
+
 --Idem que el 4, però mostrant a més el nom de departament.
