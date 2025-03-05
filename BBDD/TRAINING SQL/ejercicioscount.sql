859f1e2bd713c21a3be6583996a8ef96fee927d2
root-THC <riicardomoreenoi@gmail.com>
2025-03-05 13:21:27 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/ejercicioscount.sql
+++ BBDD/TRAINING SQL/ejercicioscount.sql
@@ -1,6 +1,6 @@
 --NUMERO DE DEPARTAMENTOS ASIGNADOS a empleados
  
-SELECT COUNT(DISTINCT deptno) -- DISTINCT es para que borre filas duplicadas
+SELECT COUNT(DISTINCT deptno) -- DISTINCT es para que borre filas duplicadas solo muestre una vez el valor
 FROM emp; 
 --NUMERO DE CAPS 
 
