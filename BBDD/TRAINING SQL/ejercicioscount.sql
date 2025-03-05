51c2edb597d4bafa8cf0c100956989001d502481
root-THC <riicardomoreenoi@gmail.com>
2025-03-05 13:40:45 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/ejercicioscount.sql
+++ BBDD/TRAINING SQL/ejercicioscount.sql
@@ -20,6 +20,8 @@ SELECT SUM(sal)
 FROM emp;
 
 --Calcular el nombre d'empleats que tenen comissió i la mitjana. Volem Mostreu tots els empleats, de manera que pretén en compte que el camp comm pot tenir nuls
+
+
 --Mostreu el salari, mínim i màxim dels empleats, agrupats per ocupació.
 --Seguint el que s'ha explicat en l'ex. 27, Mostreu per cada departament, el nombre d'empleats que tenen comissió, la suma i la mitjana.
 --Idem que el 4, però mostrant a més el nom de departament.
