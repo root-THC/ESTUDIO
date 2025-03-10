36c0cde7773a35cc8cbf296039aee239453895fe
root-THC <riicardomoreenoi@gmail.com>
2025-03-10 09:41:31 +0100

    root-THC push my project ;)

--- IAW/facturarichi.php
+++ IAW/facturarichi.php
@@ -24,7 +24,7 @@
             margin: 20px;
         }
         .articulos {
-            background-color : blue;
+            background-color : white;
             padding: 8px;
             color: white;
         }
