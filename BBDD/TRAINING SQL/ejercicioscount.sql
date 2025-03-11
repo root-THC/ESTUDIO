94d4b953df3586c85b80d8ed6823bc45d1048317
root-THC <riicardomoreenoi@gmail.com>
2025-03-11 13:58:35 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/ejercicioscount.sql
+++ BBDD/TRAINING SQL/ejercicioscount.sql
@@ -34,7 +34,7 @@ GROUP by job;
 */
 --Seguint el que s'ha explicat en l'ex. 27, Mostreu per cada departament, el nombre d'empleats que tenen comissió, la suma i la mitjana.
 
-SELECT deptno, COUNT(comm) , SUM(comm) , AVG(COALESCE(comm))
+SELECT deptno, COUNT(comm) , SUM(comm) , AVG(COALESCE(comm)) -- Para que haga la mediana de los 14 empleados si tienen valor y si es nulo
 FROM emp
 --WHERE deptNO = 10 OR deptNO = 20
 GROUP BY deptno;
