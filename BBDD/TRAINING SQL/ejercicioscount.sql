a70f07d59a8ef6f10e341957c2e72fc1b0be2435
root-THC <riicardomoreenoi@gmail.com>
2025-03-12 13:24:56 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/ejercicioscount.sql
+++ BBDD/TRAINING SQL/ejercicioscount.sql
@@ -70,7 +70,7 @@ HAVING MAX(sal)>2900;
 
 --Mostreu el salari mínim, màxim i mig dels empleats agrupats per feina, però només d'aquells la mitjana sigui superior a 4000.
 
-SELECT 
+SELECT deptno 
 
 
 --Vegeu el nombre i el nom dels departaments que tinguin més de tres empleats assignats.
