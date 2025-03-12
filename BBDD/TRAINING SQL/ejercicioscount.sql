df12fe2cba5a1c03d42a9e134693cf0d604e213c
root-THC <riicardomoreenoi@gmail.com>
2025-03-12 13:25:59 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/ejercicioscount.sql
+++ BBDD/TRAINING SQL/ejercicioscount.sql
@@ -73,7 +73,7 @@ HAVING MAX(sal)>2900;
 SELECT deptno , MIN(sal) , MAX(sal) , AVG(sal)
 FROM emp 
 GROUP by deptno 
-HAVING AVG(sal)
+HAVING AVG(sal) > 4000;
 
 
 --Vegeu el nombre i el nom dels departaments que tinguin més de tres empleats assignats.
