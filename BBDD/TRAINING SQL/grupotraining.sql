90c833456341788e26ab298eeae0ee615224dc41
root-THC <riicardomoreenoi@gmail.com>
2025-03-12 14:00:14 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/grupotraining.sql
+++ BBDD/TRAINING SQL/grupotraining.sql
@@ -7,7 +7,7 @@ FROM repventa;
 
 --2. Quin és l'import total de les comandes preses per Bill Adams?
 
-SELECT ROUND(SUM(p.importe),2) "Total Import" , 
+SELECT ROUND(SUM(p.importe),2) "Total Import" , c.nombre "Name"
 FROM pedido p
 JOIN cliente c ON p.cliecod = c.cliecod
 WHERE LOWER(c.nombre)= "bill";
