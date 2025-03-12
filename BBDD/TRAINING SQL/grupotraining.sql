4f4da8b4bbe18067f6c52f788a0288d110604678
root-THC <riicardomoreenoi@gmail.com>
2025-03-12 13:44:46 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/grupotraining.sql
+++ BBDD/TRAINING SQL/grupotraining.sql
@@ -1,5 +1,5 @@
 1. Mostrar la suma de les quotes i la suma de les vendes totals de tots els representants.
-
+/*
 2. Quin és l'import total de les comandes preses per Bill Adams?
 
 3. Calcula el preu mitjà dels productes del fabricant “aci”.
