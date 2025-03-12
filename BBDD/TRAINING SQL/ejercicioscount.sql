b2a86105f4020e31923fe1c3abcfbfb68fc6cbbd
root-THC <riicardomoreenoi@gmail.com>
2025-03-12 13:41:05 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/ejercicioscount.sql
+++ BBDD/TRAINING SQL/ejercicioscount.sql
@@ -80,7 +80,7 @@ HAVING AVG(sal) > 4000;
 
 -- Todos los campos que seleccionemos tenemos que mostrarlos en el grupo 
 
-SELECT e.deptNO "Dept Number", d.dName "Department Name"
+SELECT e.deptNO "Dept Number", d.dName "Deptment Name"
 FROM emp e
 JOIN  dept d ON e.deptNO = d.deptNO 
 GROUP BY e.deptNO , d.dName
