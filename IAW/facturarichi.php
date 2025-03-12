b205bfb705fb60b893fc8551f78da9b423e13cc1
root-THC <riicardomoreenoi@gmail.com>
2025-03-12 11:19:46 +0100

    Push F2A

--- IAW/facturarichi.php
+++ IAW/facturarichi.php
@@ -20,8 +20,9 @@
         td {
             background-color: #ffffff; 
         }
-        h1 {
+        h1,h {
             margin: 20px;
+            padding: 8px;
         }
         .articulos {
             background-color : white;
@@ -35,27 +36,44 @@
     <table>
 <!-- START STATIC TABLES -->
 <tr>
-<th>Article</th>
-<th>Quantitat</th>
-<th>Preu/u</th>
+<th>Products</th>
+<th>Quantity</th>
+<th>Price/unite</th>
 <th>Subtotal</th>
-<th>Descompte</th>
+<th>Discount</th>
 <th>Iva 21%</th>
 <th>Total</th>
 
 <!-- END STATIC TABLES -->       
   
     <?php   
-$numero_quantitat = rand (1,100);
     for ($i = 1; $i <= 10; $i++) {  
-        echo "<tr> <th class='articulos'>Article $i</th> </tr>";
-    }
-    for ($a = 1; $i <= 10; ) {  
-        echo "<tr> <th class='articulos'>Article $i</th> </tr>";
-    }
-
-?> 
+        $quantity_number = rand (1,100);
+        $price = rand (50,600);
+        $subtotal = $quantity_number * $price;
+    
+        echo "<tr>"; 
+        echo "<td>Article $i</td>" ;
+        # Camp Quantitat
+        if ($quantity_number==1){
+            echo "<td> $quantity_number ud</td>";
+        } elseif ( $quantity_number==0){
+            echo "<td> No hay existencias </td>";
+        } else {
+            echo "<td>$quantity_number uds</td>";
+        }
+        # Camp Price
+        echo "<td>Preu $price €</td>";
+        echo "<td>Preu $subtotal €</td>";
+        echo  "</tr>";
+                                }
+    ?> 
   </table>
+  <?php 
+  #$total_productos = ;
+  echo "<h>Total products:" . array_sum . "</h>";
+  ?>
+
 
 </body>
 </html>
