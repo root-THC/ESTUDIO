57fecec87e8491b89ccf17d8275b4eba37a88a74
root-THC <riicardomoreenoi@gmail.com>
2025-03-14 11:37:48 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/grupotraining.sql
+++ BBDD/TRAINING SQL/grupotraining.sql
@@ -9,8 +9,7 @@ FROM repventa;
 
 SELECT ROUND(SUM(p.importe),2) "Total Import" , c.nombre "Name"
 FROM pedido p
-JOIN cliente c ON p.cliecod = c.cliecod
-WHERE LOWER(c.nombre)= "bill";
+JOIN cliente c ON p.cliecod = c.cliecod;
 
 
 
