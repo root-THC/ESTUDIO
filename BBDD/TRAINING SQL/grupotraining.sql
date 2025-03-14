0df3529772f0369573506b0ca827dd3d0dfb7714
root-THC <riicardomoreenoi@gmail.com>
2025-03-14 11:50:08 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/grupotraining.sql
+++ BBDD/TRAINING SQL/grupotraining.sql
@@ -10,7 +10,6 @@ FROM repventa;
 SELECT ROUND(SUM(p.importe),2)||'€' "Total Import" , r.nombre "Name"
 FROM pedido p
 JOIN repventa r ON p.repcod = r.repcod
-GROUP BY p.importe, r.nombre
 WHERE LOWER(r.nombre)='bill adams'
 GROUP BY p.importe, r.nombre;
 
