0ea4c712782f2cf7e28042da82665b4de92238d5
root-THC <riicardomoreenoi@gmail.com>
2025-03-17 09:55:15 +0100

    root-THC push my project ;)

--- IAW/facturarichi.php
+++ IAW/facturarichi.php
@@ -60,7 +60,7 @@
         $subtotal = $quantity_number * $price;
         $subtotal_withiva = $subtotal * 1.21;
 
-        $total_products1 += $quantity_number;
+        $total_products += $quantity_number;
         $total_noiva += $subtotal ;
         $total_withiva += $subtotal_withiva;
         $price_discount5 = $subtotal * 0.05;
