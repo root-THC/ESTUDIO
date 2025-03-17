9623210bc4c8fedd96b6aad67ed4cf744ad9e2ab
root-THC <riicardomoreenoi@gmail.com>
2025-03-17 09:41:41 +0100

    root-THC push my project ;)

--- SCRIPTS BASH/sqlauto.sh
+++ SCRIPTS BASH/sqlauto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker compose up -d
-$docker_id = docker id awk '{print $1}'
+docker_id = docker id awk '{print $1}'
 docker exec -it $docker_id /bin/bash
 psql --username=richi template1
