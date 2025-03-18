ba5d43969061f0ecf375ce662f4688177f8ee786
root-THC <riicardomoreenoi@gmail.com>
2025-03-18 14:00:20 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/subconsultas.sql
+++ BBDD/TRAINING SQL/subconsultas.sql
@@ -18,7 +18,7 @@ WHERE hiredate = (SELECT MIN(hiredate) FROM emp);
 -- ANIDAMIENTO DE FUNCIONES DE GRUPOS 
 
 SELECT MAX(campotemportal) 
-FROM (SELECT AVG(sal)campotemportal 
-FROM emp
+FROM 
+(SELECT AVG(sal)campotemportal FROM emp
 GROUP BY deptno) nombretablatemporal ;
 
