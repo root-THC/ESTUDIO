8c1a39142559a42ca42391936cca2ae6aa3da21e
root-THC <riicardomoreenoi@gmail.com>
2025-03-19 13:04:54 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/grupotraining.sql
+++ BBDD/TRAINING SQL/grupotraining.sql
@@ -74,6 +74,6 @@ FROM repventa;
 
 SELECT ofinum, SUM(quotes) , SUM(ventas)
 FROM repventa
-GROUP BY of
+GROUP BY ofinum;
 
 --21. Mostra el nombre de comandes que superen el 75% de les existències.
