0032c15a1fbe164a66957e11f6c9780a68c30f02
root-THC <riicardomoreenoi@gmail.com>
2025-03-19 12:18:57 +0100

    root-THC push my project ;)

--- SCRIPTS BASH/sqlauto.sh
+++ SCRIPTS BASH/sqlauto.sh
@@ -2,5 +2,5 @@
 cd '/home/users/inf/hisx1/a241535rm/Downloads/postgres-docker-arreglat'
 docker rm -f $(docker ps -aq)
 docker compose up -d
-docker_id=$(docker ps | awk '{print $1}')
+docker_id=$(docker ps | awk '{$1}')
 docker exec -it "${docker_id}"  /bin/bash -c "psql --username=richi template1"
