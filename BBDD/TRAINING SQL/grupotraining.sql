f0f18a4f5f1ccc363f845c9f24f0813f70a0afa8
root-THC <riicardomoreenoi@gmail.com>
2025-03-19 12:24:39 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/grupotraining.sql
+++ BBDD/TRAINING SQL/grupotraining.sql
@@ -19,8 +19,8 @@ FROM pedido p
 JOIN producto pr ON (p.fabcod,p.prodcod) = (pr.fabcod,pr.prodcod)
 WHERE LOWER(p.fabcod)='aci'
 GROUP BY p.fabcod;
-/*
-4. Quin és l'import mitjà de la comanda sol·licitada pel client "acme mfg."
+
+--4. Quin és l'import mitjà de la comanda sol·licitada pel client "acme mfg."
 /*
 5. Mostrar la quota màxima i la quota mínima de les quotes dels representants.
 
