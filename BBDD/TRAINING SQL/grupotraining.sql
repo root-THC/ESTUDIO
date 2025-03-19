ee1968b0dea4be24684733ac00867868fc824221
root-THC <riicardomoreenoi@gmail.com>
2025-03-19 13:39:37 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/grupotraining.sql
+++ BBDD/TRAINING SQL/grupotraining.sql
@@ -73,7 +73,7 @@ r.nombre ,
 SUM(importe)
 FROM pedido p
 JOIN cliente c ON = p.cliecod = c.cliecod
-JOIN repventa r ON r.repventa = p.repcod
+JOIN repventa r ON r.repcod = p.repcod
 GROUP BY c.nombre, r.nombre;
 
 
