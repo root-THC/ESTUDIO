ea6a7edd807d187400e23b56fc859f1f53c6cf75
root-THC <riicardomoreenoi@gmail.com>
2025-03-21 11:27:49 +0100

    root-THC push my project ;)

---
+++ BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
@@ -0,0 +1,29 @@
+Mostrar el nom i el lloc dels que són cap (ja està fet amb self join, ara amb subconsultes)
+1. Obtenir una llista dels representants les quotes dels quals són iguals o superiors a lobjectiu de loficina dAtlanta.
+
+2. Obtenir una llista de tots els clients (nom) que van ser contactats per primera vegada per Bill Adams.
+
+3. Obtenir una llista de tots els productes del fabricant ACI les del qual existències superen les existències del producte 41004 del mateix fabricant.
+
+4. Obtenir una llista dels representants que treballen a les oficines que han aconseguit superar el seu objectiu de vendes.
+
+5. Obtenir una llista dels representants que no treballen a les oficines dirigides per Larry Fitch.
+
+6. Obtenir una llista de tots els clients que han demanat comandes del fabricant ACI entre gener i juny del 2003.
+
+7. Obtenir una llista dels productes dels quals s'ha pres una comanda de 150 euros o més.
+
+8. Obtenir una llista dels clients contactats per Sue Smith que no han sol·licitat comandes amb imports superiors a 18 euros.
+
+9. Obtenir una llista de les oficines on hi hagi algun representant la quota del qual sigui més del 55% de l'objectiu de l'oficina. Per comprovar el vostre exercici, feu una Consulta prèvia el resultat de la qual valideu l'exercici.
+
+10. Obtenir una llista dels representants que han pres alguna comanda l'import del qual sigui més del 10% de la seva quota.
+
+11. Obtenir una llista de les oficines on el total de vendes dels seus representants han aconseguit un import de vendes que supera el 50% de lobjectiu de loficina. Mostrar també l'objectiu de cada oficina (suposeu que el camp vendes d'oficina no existeix).
+
+12. Quina és la descripció del primer producte sol·licitat en una comanda? 13. Quin representant té el millor percentatge de vendes?
+
+14. Quin representant té el pitjor percentatge de vendes?
+
+Quin producte (Descripció) té més comandes?
+16 . Quin producte s'ha venut més?
