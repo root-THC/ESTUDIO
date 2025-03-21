e9667f52c52887c4e05bf78179d8878679399a2c
root-THC <riicardomoreenoi@gmail.com>
2025-03-21 12:12:50 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
+++ BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
@@ -13,6 +13,9 @@ MUESTRA LOS DEPARTAMENTOS QUE NO TIENEN ASIGNADO NINGÚN JEFE
 Esto es una resta para 2 tablas tenemos que restar los que si sabemos que estan asignados
 a los que no estan asignados
 
+IN 
+ANY = ALGUNO
+ALL = TODOS
 SELECT deptno 
 FROM dept
 WHERE deptno 
