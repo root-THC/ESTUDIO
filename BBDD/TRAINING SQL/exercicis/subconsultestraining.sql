997dacfaa85f18389ac802de85ad12575e2c1823
root-THC <riicardomoreenoi@gmail.com>
2025-03-21 11:30:07 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
+++ BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
@@ -1,7 +1,7 @@
 --MULTIREGISTRE 
 
 /*
-SELECT nombre p
+SELECT nombre , puesto 
 0. Mostrar el nom i el lloc dels que són cap (ja està fet amb self join, ara amb subconsultes)
 1. Obtenir una llista dels representants les quotes dels quals són iguals o superiors a lobjectiu de loficina dAtlanta.
 
