a4856d9b71a8cce6c82f144bbd83860f044f5837
root-THC <riicardomoreenoi@gmail.com>
2025-03-25 13:33:33 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
+++ BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
@@ -71,7 +71,7 @@ JOIN oficina o ON r.ofinum = o.ofinum
 WHERE cuota > 0.55*objetivo;
 
 
-SELECT ciu
+SELECT ciudad FROM oficina 
 
 
 10. Obtenir una llista dels representants que han pres alguna comanda l'import del qual sigui més del 10% de la seva quota.
