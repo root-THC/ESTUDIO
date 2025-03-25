76f51cc235c410d681d061b0faf0f002e637f524
root-THC <riicardomoreenoi@gmail.com>
2025-03-25 13:50:00 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
+++ BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
@@ -90,7 +90,7 @@ Quin producte (Descripció) té més comandes?
 
 SELECT descrip
 FROM producto 
-WHERE pednum NOT IN (SELECT pednum FROM pedido)
+WHERE pednum NOT IN (SELECT pednum FROM pedido);
 
 
 SELECT pednum
