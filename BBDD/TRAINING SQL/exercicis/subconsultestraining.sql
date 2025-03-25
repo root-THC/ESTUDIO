d68a2f649b24d73298f8787beb32d2833bac37de
root-THC <riicardomoreenoi@gmail.com>
2025-03-25 13:37:11 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
+++ BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
@@ -75,6 +75,7 @@ SELECT ciudad FROM oficina o WHERE objetivo <
 (SELECT cuota*0.55 FROM repventa r WHERE r.ofinum=o.ofinum);
 
 
+
 10. Obtenir una llista dels representants que han pres alguna comanda l'import del qual sigui més del 10% de la seva quota.
 
 11. Obtenir una llista de les oficines on el total de vendes dels seus representants han aconseguit un import de vendes que supera el 50% de lobjectiu de loficina. Mostrar també l'objectiu de cada oficina (suposeu que el camp vendes d'oficina no existeix).
