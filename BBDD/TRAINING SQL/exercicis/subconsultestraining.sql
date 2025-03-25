9ee727224ed5850d6ceea19122be90be8c6d620f
root-THC <riicardomoreenoi@gmail.com>
2025-03-25 10:38:48 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
+++ BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
@@ -24,7 +24,7 @@ NOT IN (SELECT deptno FROM emp);
 
  -- EN VEZ DE NOT IN PODRÍA SER != ALL SIGNIFICA DIFERENTE DE 
 
-SELECT ename, sal,
+SELECT ename, sal, deptno
 
 
  Mostra el nom del empleats que són cap
