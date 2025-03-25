e383d38d05d0c0dbfb475bdab309993ab6673cb4
root-THC <riicardomoreenoi@gmail.com>
2025-03-25 10:35:19 +0100

    root-THC push my project ;)

--- BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
+++ BBDD/TRAINING SQL/exercicis/subconsultestraining.sql
@@ -35,7 +35,8 @@ Los que no son jefes
 
 SELECT ename 
  FROM emp
- WHERE empno NOT IN (SELECT mgr FROM emp WHERE mgr IS NOT NULL); EL NULL ES IMPORTANTE PORQUE AL DEVOLVER UN NULL QUE ES PRESIDENTE
+ WHERE empno NOT IN (SELECT mgr FROM emp WHERE mgr IS NOT NULL); 
+ --EL NULL ES IMPORTANTE PORQUE AL DEVOLVER UN NULL QUE ES PRESIDENTE
 
 
 0. Mostrar el nom i el lloc dels que són cap (ja està fet amb self join, ara amb subconsultes)
