4a298464d1a49251ca580fbe76c9fc35bd95ad4f
root-THC <riicardomoreenoi@gmail.com>
2025-03-26 13:16:43 +0100

    root-THC push my project ;)

--- BBDD/SQLS/a241535rm.sql
+++ BBDD/SQLS/a241535rm.sql
@@ -15,6 +15,6 @@ JOIN SOCI s ON l.codsoci = s.codsoci
 GROUP BY s.codsoci;
 
 
-SELECT COUNT(datapres),SUM(import) , nom
+SELECT COUNT(datapres),SUM(import) , s.nom
 FROM LLOGUER l
 JOIN SOCI s ON l.codsoci = s.codsoci;
