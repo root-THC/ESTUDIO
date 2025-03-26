0be60cb68ae4c20cbf849c81d69709f1e6b6ffee
root-THC <riicardomoreenoi@gmail.com>
2025-03-26 13:34:10 +0100

    root-THC push my project ;)

--- BBDD/SQLS/a241535rm.sql
+++ BBDD/SQLS/a241535rm.sql
@@ -1,6 +1,8 @@
 -- Ricardo Moreno Ianiva
 -- a241535rm.sql
 
+
+--1
 SELECT p.codpeli , d.coddvd , titol , nom , cognoms , datapres 
 FROM LLOGUER l 
 JOIN SOCI s ON l.codsoci = s.codsoci    
@@ -9,15 +11,11 @@ JOIN pelicula p ON d.codpeli = p.codpeli
 ORDER BY 1,6;
 
 
-SELECT COALESCE(COUNT(datapres),0) , s.CodSOCI || cognoms , import 'Despesa Realitzada'
-FROM LLOGUER l
-JOIN SOCI s ON l.codsoci = s.codsoci
-GROUP BY s.codsoci;
 
 
 SELECT s.codsoci , s.nom || ', ' || s.cognoms Soci , COALESCE(COUNT(datapres),0) "Prestecs Realitzats" , SUM(import) || ' €' "Despeses realitzades"
 FROM LLOGUER l
 RIGHT JOIN SOCI s ON l.codsoci = s.codsoci
 GROUP BY s.nom,s.codsoci , s.cognoms
-ORDER BY 4 DESC ,3, s.cognoms ;
+ORDER BY 4 DESC,3 , s.cognoms;
 
