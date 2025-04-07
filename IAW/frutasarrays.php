ffbc7d577791a9082ab83b2923050450a421e3ef
root-THC <riicardomoreenoi@gmail.com>
2025-04-07 09:27:57 +0200

    root-THC push my project ;)

--- IAW/frutasarrays.php
+++ IAW/frutasarrays.php
@@ -161,7 +161,7 @@ foreach ($productos as $producto) {
 echo "</table>";
 
 echo "<p> Total productos : $total_articulos</p>";
-echo "<p> Total (NO IVA) :" . number_format($total_sin_iva,2) .  "€</p>";
+echo "<p> Total (NO IVA) :" . number_format($total_sin_iva,2) .  " €</p>";
 echo "<p> Total (CON IVA) : $precio_con_iva</p>";
 
 ?>
