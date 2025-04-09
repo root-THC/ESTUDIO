7198cadd0e295ecca1fa71f1c61b747851f3a659
root-THC <riicardomoreenoi@gmail.com>
2025-04-09 12:39:53 +0200

    root-THC push my project ;)

---
+++ BBDD/M10/ejroles.sql
@@ -0,0 +1,34 @@
+Ejercicio 1: Crear roles con y sin login
+
+1.1. Crear usuario ana con contraseña y con login
+1.2. Crear rol de grupo "desarrolladores" (sin login)
+
+1.3. Añadir el usuario al grupo 
+
+
+2. Crear esquemas y asignar propietario
+
+2.1. Crear esquema llamado "proyecto" propiedad del rol desarrolladores
+
+
+3. Controlar privilegios en el esquema
+
+3.1. Permitir uso y creación de objetos en el esquema desarrolladores
+3.2. Revocar permisos públicos
+
+4. Asignar permisos sobre tablas
+
+4.1. Crear la tabla clientes dentro del esquema proyecto
+
+La estructura es:   id SERIAL PRIMARY KEY,  nombre TEXT,  email TEXT
+
+4.2. Crear rol usuario_consulta solo para lectura
+4.3. Dar permisos de lectura sobre la tabla cliente al usuario_consulta
+
+ 5. Cambio de role sin tener que salir de la sesion
+
+5.1. Cambiar al rol ana
+
+5.2. Crear una tabla llamada "prueba" dentro del esquema proyecto como ana
+
+5.3. Volver al rol original
