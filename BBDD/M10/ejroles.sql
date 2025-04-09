8ae167314bce102bca8a3dbaf2e33b577fa2eb6b
root-THC <riicardomoreenoi@gmail.com>
2025-04-09 13:07:09 +0200

    root-THC push my project ;)

--- BBDD/M10/ejroles.sql
+++ BBDD/M10/ejroles.sql
@@ -28,6 +28,10 @@ CREATE SCHEMA proyecto AUTHORIZATION desarrolladores;
 
 3. Controlar privilegios en el esquema
 
+
+
+/*
+
 3.1. Permitir uso y creación de objetos en el esquema desarrolladores
 3.2. Revocar permisos públicos
 
