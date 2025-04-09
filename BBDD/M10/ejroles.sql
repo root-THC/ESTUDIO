1cb93bc0a04f848ffa2fea289c752824325d1234
root-THC <riicardomoreenoi@gmail.com>
2025-04-09 13:06:25 +0200

    root-THC push my project ;)

--- BBDD/M10/ejroles.sql
+++ BBDD/M10/ejroles.sql
@@ -20,9 +20,9 @@ GRANT desarrolladores TO ana;
 
 
 
-/*
-2.1. Crear esquema llamado "proyecto" propiedad del rol desarrolladores
 
+--2.1. Crear esquema llamado "proyecto" propiedad del rol desarrolladores
+/*
 
 3. Controlar privilegios en el esquema
 
