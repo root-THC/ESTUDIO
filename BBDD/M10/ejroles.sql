03f55be23041c46e991c1a2c2e02da802d4c28e9
root-THC <riicardomoreenoi@gmail.com>
2025-04-11 11:42:29 +0200

    root-THC push my project ;)

--- BBDD/M10/ejroles.sql
+++ BBDD/M10/ejroles.sql
@@ -66,7 +66,7 @@ CREATE ROLE usuario_consulta LOGIN;
 --4.3. Dar permisos de lectura sobre la tabla cliente al usuario_consulta
 
 
-GRANT UPDATE ON proyecto.clientes TO usuario_consulta;
+GRANT SELECT ON proyecto.clientes TO usuario_consulta;
 
 
 
