abd95c58fb7818795127645ffaacb454be47b506
root-THC <riicardomoreenoi@gmail.com>
2025-04-30 13:31:21 +0200

    root-THC push my project ;)

--- BBDD/M10/funcionstraining.sql
+++ BBDD/M10/funcionstraining.sql
@@ -45,7 +45,7 @@ BEGIN
     FROM cliente
     WHERE cliecod = p_cliecod AND repcod = p_repcod AND limcred = p_limcred;
     
-    IF 
+    IF cliecod 
 RETURN 'Client ' || v_cliente.repcod || ' s''ha donat d''alta correctament ';
 
 EXCEPTION 
