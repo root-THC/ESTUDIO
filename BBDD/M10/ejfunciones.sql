a6329647a9ca50853d9120420cb112df24926569
root-THC <riicardomoreenoi@gmail.com>
2025-04-30 12:59:18 +0200

    root-THC push my project ;)

--- BBDD/M10/ejfunciones.sql
+++ BBDD/M10/ejfunciones.sql
@@ -30,3 +30,31 @@ END;
 $$LANGUAGE PLPGSQL;
 
 -- 2do EJERCICIO J
+
+
+CREATE OR REPLACE FUNCTION existeixClient(p_cliecod SMALLINT)
+RETURNS BOOLEAN
+AS $$
+
+
+DECLARE
+
+   v_cliente record;
+   
+
+BEGIN
+    
+    SELECT * 
+    INTO STRICT  v_cliente
+    FROM cliente
+    WHERE cliecod = p_cliecod;
+    
+RETURN 'TRUE';
+
+EXCEPTION 
+    WHEN NO_DATA_FOUND THEN
+    RETURN 'FALSE';
+   
+END;
+
+$$LANGUAGE PLPGSQL;
