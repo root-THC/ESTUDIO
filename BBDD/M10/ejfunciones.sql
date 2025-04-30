fc688b0564a637828c7ee4e9adcd24b62aa01807
root-THC <riicardomoreenoi@gmail.com>
2025-04-30 12:43:15 +0200

    root-THC push my project ;)

--- BBDD/M10/ejfunciones.sql
+++ BBDD/M10/ejfunciones.sql
@@ -15,20 +15,7 @@ BEGIN
     FROM cliente
     WHERE cliecod = p_cliecod; 
 
-    IF 
-        LOWER(v_emp.job)= 'salesman' THEN v_sal := v_emp.sal * 1.1;
-    ELSE 
-        ENDIF;
-    IF
-        LOWER(v_emp.job)= 'clerk' THEN v_sal := v_emp.sal * 1.2;
-    ELSE
-        ENDIF; 
-    IF 
-        v_sal := v_emp.sal * 1.3;
-    
    
-    
-    RETURN  EXISTS ();
 
 -- EL EXCEPTION PARA QUE CUANDO NO EXISTA EL VALOR MUESTRE UN MENSAJE
 
