bcd0024821d222e08aa0b8cf982bb4c3fa45c26b
root-THC <riicardomoreenoi@gmail.com>
2025-04-30 12:43:38 +0200

    root-THC push my project ;)

--- BBDD/M10/ejfunciones.sql
+++ BBDD/M10/ejfunciones.sql
@@ -16,13 +16,6 @@ BEGIN
     WHERE cliecod = p_cliecod); 
 
    
-
--- EL EXCEPTION PARA QUE CUANDO NO EXISTA EL VALOR MUESTRE UN MENSAJE
-
-EXCEPTION 
-    WHEN NO_DATA_FOUND THEN
-    RETURN 'Error no n''hi ha cap valor que coincideixi amb la teva cerca : ' || p_empno;
-
 END;
 
 $$LANGUAGE PLPGSQL;
