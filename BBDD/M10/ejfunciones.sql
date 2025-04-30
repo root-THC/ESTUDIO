9eb3d62ca01276aba0464fd92a4d2899d62893e8
root-THC <riicardomoreenoi@gmail.com>
2025-04-30 13:08:13 +0200

    root-THC push my project ;)

--- BBDD/M10/ejfunciones.sql
+++ BBDD/M10/ejfunciones.sql
@@ -31,7 +31,7 @@ $$LANGUAGE PLPGSQL;
 
 -- 2do EJERCICIO J
 
-CREATE SEQUENCE cliecod
+CREATE SEQUENCE cliecod_seq 
 CREATE OR REPLACE FUNCTION altaClient(p_nombre,p,repcod,p_limcred SMALLINT)
 RETURNS BOOLEAN
 AS $$
