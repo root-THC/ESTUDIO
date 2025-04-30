d9183ccc2295bc3f9b02f48c6e2ca833e873c951
root-THC <riicardomoreenoi@gmail.com>
2025-04-30 12:26:25 +0200

    root-THC push my project ;)

---
+++ BBDD/M10/ejfunciones.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE FUNCTION mostraEmpleat(p_empno SMALLINT)
+RETURNS VARCHAR
+AS $$
+
+-- EL RECORD ES PARA GUARDAR X VALORES
+DECLARE
+
+   v_emp record; -- variable compuesta
+   v_sal emp.sal%type
+
+BEGIN
+
+    SELECT * 
+    INTO STRICT v_emp -- la variable de antes
+    FROM emp
+    WHERE empno = p_empno; -- parametro del codigo del empleado
+
+    IF 
+        LOWER(v_emp.job)= 'salesman' THEN v_sal := v_emp.sal * 1.1;
+    ELSE 
+        ENDIF;
+    IF
+        LOWER(v_emp.job)= 'clerk' THEN v_sal := v_emp.sal * 1.2;
+    ELSE
+        ENDIF; 
+    IF 
+        v_sal := v_emp.sal * 1.3;
+    
+   
+    
+    RETURN  'L''empleat ' || v_emp.ename || ' treballa de ' || v_emp.job;
+
+-- EL EXCEPTION PARA QUE CUANDO NO EXISTA EL VALOR MUESTRE UN MENSAJE
+
+EXCEPTION 
+    WHEN NO_DATA_FOUND THEN
+    RETURN 'Error no n''hi ha cap valor que coincideixi amb la teva cerca : ' || p_empno;
+
+END;
+
+$$LANGUAGE PLPGSQL;
