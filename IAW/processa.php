611e15d9b35f235a3292584419bda8ae439506c9
root-THC <riicardomoreenoi@gmail.com>
2025-05-05 09:15:24 +0200

    root-THC push my project ;)

--- IAW/processa.php
+++ IAW/processa.php
@@ -8,7 +8,7 @@
     <h1>Processar el formulari</h1>
     <p>Coses que m'arriben:</p>
     <?php
-        echo "<p>Nom (de \$_GET): " . $_GET['nom'] . "</p>"; <! -->
+        echo "<p>Nom (de \$_GET): " . $_GET['nom'] . "</p>"; <!--  -->
         echo "<p>Nom (de \$_POST): " . $_POST['nom'] . "</p>";
         echo "<p>Nom (de \$_REQUEST): " . $_REQUEST['nom'] . "</p>";
     ?>
