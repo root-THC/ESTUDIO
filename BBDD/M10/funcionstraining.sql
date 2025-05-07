9db1348e706d557459ea3ca20203fffe9caeab1b
root-THC <riicardomoreenoi@gmail.com>
2025-05-07 13:12:42 +0200

    root-THC push my project ;)

--- BBDD/M10/funcionstraining.sql
+++ BBDD/M10/funcionstraining.sql
@@ -117,6 +117,7 @@ AS $$
             IF NOT existeixClient(p_cliecod) THEN
                 RETURN 'No existe el cliente '||p_cliecod||;
             END IF;
+            
             IF NOT stock0k (p_cant,p_fabcod ,p_prodcod ) THEN
                 RETURN 'No existe el cliente '||p_cliecod||;
             END IF;
