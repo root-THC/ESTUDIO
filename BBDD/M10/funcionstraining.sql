3af4372e1ac42cbfccbdbdb1f2c7076b4dc4c0ef
root-THC <riicardomoreenoi@gmail.com>
2025-05-07 12:24:33 +0200

    root-THC push my project ;)

--- BBDD/M10/funcionstraining.sql
+++ BBDD/M10/funcionstraining.sql
@@ -91,7 +91,7 @@ CREATE OR REPLACE FUNCTION stock0k(p_cant SMALLINT,p_fabcod CHAR,p_prodcod CHAR)
 
 -- 4 to ej J
 
-CREATE OR REPLACE FUNCTION altaComanda(p_cliecod,)
+CREATE OR REPLACE FUNCTION altaComanda(p_cliecod SMALLINT , )
     RETURNS VARCHAR
     AS $$     
     DECLARE 
