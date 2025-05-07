519f4b4dd90ae6ee8e84be3d2ce1b2c423d2b6dc
root-THC <riicardomoreenoi@gmail.com>
2025-05-07 12:57:35 +0200

    root-THC push my project ;)

--- BBDD/M10/funcionstraining.sql
+++ BBDD/M10/funcionstraining.sql
@@ -105,6 +105,7 @@ CREATE OR REPLACE FUNCTION altaComanda(p_cliecod SMALLINT,p_cant SMALLINT,p_fabc
 
     IF existeixClient(p_cliecod) THEN 
         IF stock0k (p_cant,p_fabcod ,p_prodcod ) THEN 
+        
     ELSE
 
     SELECT 
