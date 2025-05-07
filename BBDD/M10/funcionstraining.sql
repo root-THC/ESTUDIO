41082f461db19cf74dddeb75b998006b1b404921
root-THC <riicardomoreenoi@gmail.com>
2025-05-07 12:52:01 +0200

    root-THC push my project ;)

--- BBDD/M10/funcionstraining.sql
+++ BBDD/M10/funcionstraining.sql
@@ -98,7 +98,7 @@ CREATE OR REPLACE FUNCTION altaComanda(p_cliecod SMALLINT,p_cant SMALLINT,p_fabc
     v_productos record;
     BEGIN 
     SELECT *
-    INTO  
+    INTO STRICT  
     FROM producto
     WHERE fabcod = p_fabcod AND prodcod = p_prodcod;
 
