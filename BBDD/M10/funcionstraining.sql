515090ceffa85e8a40a42a66a398d99a6731226a
root-THC <riicardomoreenoi@gmail.com>
2025-05-07 13:11:20 +0200

    root-THC push my project ;)

--- BBDD/M10/funcionstraining.sql
+++ BBDD/M10/funcionstraining.sql
@@ -114,7 +114,7 @@ AS $$
             RETURN 1;
         END IF;  
         */
-            IF existeixClient(2111) THEN
+            IF NOT existeixClient(2111) THEN
 
     END; 
     $$ LANGUAGE PLPGSQL;
