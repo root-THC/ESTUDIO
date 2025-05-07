b83f773b56cbfbcb1ba4101fb687b508a78be36f
root-THC <riicardomoreenoi@gmail.com>
2025-05-07 13:27:48 +0200

    root-THC push my project ;)

--- BBDD/M10/funcionstraining.sql
+++ BBDD/M10/funcionstraining.sql
@@ -105,7 +105,7 @@ AS $$
 
         IF existeixClient(p_cliecod) THEN 
             IF stock0k (p_cant,p_fabcod ,p_prodcod ) THEN 
-                v_importe := v_productos.precio * p_cant INTO STRICT v_importe;
+                v_importe := v_productos.precio * p_cant;
                 RETURN 'El pedido de ' || p_cliecod || ' es apto '|| 'el importe del pedido es de '|| v_importe || '€' ;
             ELSE
                 RETURN 'El cliente ' || p_cliecod || ' existe pero no disponemos de suficientes unidades solo disponemos de ' || v_productos.exist ;
