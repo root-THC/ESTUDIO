c278b2783d5b05cb46dff0ac9a99e86948b39cdb
root-THC <riicardomoreenoi@gmail.com>
2025-05-07 13:14:41 +0200

    root-THC push my project ;)

--- BBDD/M10/funcionstraining.sql
+++ BBDD/M10/funcionstraining.sql
@@ -106,7 +106,7 @@ AS $$
         IF existeixClient(p_cliecod) THEN 
             IF stock0k (p_cant,p_fabcod ,p_prodcod ) THEN 
                 SELECT v_productos.precio * p_cant INTO STRICT v_importe;
-                RETURN v_importe;
+                RETURN Ev_importe;
             ELSE
                 RETURN 1;
             END IF;        
