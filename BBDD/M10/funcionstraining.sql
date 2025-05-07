008a5cd149b1489c2e432982bbf3a03f58dff4cc
root-THC <riicardomoreenoi@gmail.com>
2025-05-07 12:51:30 +0200

    root-THC push my project ;)

--- BBDD/M10/funcionstraining.sql
+++ BBDD/M10/funcionstraining.sql
@@ -99,6 +99,7 @@ CREATE OR REPLACE FUNCTION altaComanda(p_cliecod SMALLINT,p_cant SMALLINT,p_fabc
     BEGIN 
     SELECT * 
     FROM producto
+    WHERE 
 
     IF existeixClient(p_cliecod) THEN 
 
