d6e9df6cec095f4eaf4be6dc16980a8aa89f5987
root-THC <riicardomoreenoi@gmail.com>
2025-05-07 12:54:35 +0200

    root-THC push my project ;)

--- BBDD/M10/funcionstraining.sql
+++ BBDD/M10/funcionstraining.sql
@@ -96,7 +96,7 @@ CREATE OR REPLACE FUNCTION altaComanda(p_cliecod SMALLINT,p_cant SMALLINT,p_fabc
     AS $$     
     DECLARE 
     v_productos record;
-    v_
+    v_existencias_suficientes =
     BEGIN 
     SELECT *
     INTO STRICT v_productos; 
