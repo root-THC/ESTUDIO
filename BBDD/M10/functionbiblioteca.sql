fa4fab2117d18416a82745fef4fd41b4d3268403
root-THC <riicardomoreenoi@gmail.com>
2025-05-14 13:33:29 +0200

    root-THC push my project ;)

--- BBDD/M10/functionbiblioteca.sql
+++ BBDD/M10/functionbiblioteca.sql
@@ -47,7 +47,7 @@ BEGIN
 
     JOIN usuari u ON p.idUsuari = u.idUsuari
 
-    JOIN exemplar e ON e.idDocument =  
+    JOIN exemplar e ON e.idDocument = d.idDocument 
     
     JOIN document d 
     
