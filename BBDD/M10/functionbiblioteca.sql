02b59c487540efe02cf4779c1d95ee6a434f5ac2
root-THC <riicardomoreenoi@gmail.com>
2025-05-14 13:08:51 +0200

    root-THC push my project ;)

--- BBDD/M10/functionbiblioteca.sql
+++ BBDD/M10/functionbiblioteca.sql
@@ -13,7 +13,7 @@ BEGIN
     FROM usuari
     WHERE idusuari = p_usuari;
     
-    IF bloquejat THEN
+    IF v_bloquejat THEN
         RETURN '1:usuari bloquejat';
     ELSE 
         RETURN '0:usuari no té cap bloqueig i pot fer el prèstec';
