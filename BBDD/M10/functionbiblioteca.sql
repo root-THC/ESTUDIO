2fcd6b33b01d1fda92a3871b09387771eeb7e65c
root-THC <riicardomoreenoi@gmail.com>
2025-05-14 13:11:53 +0200

    root-THC push my project ;)

--- BBDD/M10/functionbiblioteca.sql
+++ BBDD/M10/functionbiblioteca.sql
@@ -27,3 +27,31 @@ EXCEPTION
 END;
 
 $$LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE FUNCTION usuariOK(p_usuari INTEGER)
+RETURNS VARCHAR
+AS $$
+
+DECLARE
+    v_bloquejat usuari.bloquejat%TYPE;
+
+BEGIN
+    SELECT bloquejat 
+    INTO STRICT  v_bloquejat
+    FROM usuari
+    WHERE idusuari = p_usuari;
+    
+    IF v_bloquejat THEN
+        RETURN '1:usuari bloquejat';
+    ELSE 
+        RETURN '0:usuari no té cap bloqueig i pot fer el prèstec';
+    END IF;
+    
+
+EXCEPTION 
+    WHEN NO_DATA_FOUND THEN
+    RETURN '2: usuari ' || p_usuari || ' inexistent'; 
+
+END;
+
+$$LANGUAGE PLPGSQL;
