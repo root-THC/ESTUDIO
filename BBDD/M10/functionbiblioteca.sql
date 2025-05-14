3fc21a94a406f0b47d7e7b494510bf2969a5bedf
root-THC <riicardomoreenoi@gmail.com>
2025-05-14 13:39:29 +0200

    root-THC push my project ;)

--- BBDD/M10/functionbiblioteca.sql
+++ BBDD/M10/functionbiblioteca.sql
@@ -41,7 +41,7 @@ DECLARE
     v_bloquejat usuari.bloquejat%TYPE;
 
 BEGIN
-    SELECT u.idusuari , d.idcoument , p.datadev
+    SELECT u.idusuari , d.idDoument , p.datadev
     
     FROM prestec p
 
