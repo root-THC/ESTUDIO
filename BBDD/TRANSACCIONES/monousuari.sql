7ccc2ad57aa5cc835a21a918bd293599a896d68f
root-THC <riicardomoreenoi@gmail.com>
2025-05-21 12:29:51 +0200

    root-THC push my project ;)

--- BBDD/TRANSACCIONES/monousuari.sql
+++ BBDD/TRANSACCIONES/monousuari.sql
@@ -11,7 +11,7 @@ UPDATE punts SET valor = 4 WHERE id = 10;
 ROLLBACK;
 SELECT valor FROM punts WHERE id = 10;
 ```
-Mostrará
+Mostrará 5
 
 2. Analitzant les següents sentències explica quins canvis es realitzen i on es realitzen. Finalment digues quin valor s'obtindrà amb l'últim SELECT.  
 
