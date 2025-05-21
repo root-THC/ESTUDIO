7ab24f223359db0365c78c4a130e89a7a4ebaf33
root-THC <riicardomoreenoi@gmail.com>
2025-05-21 12:41:13 +0200

    root-THC push my project ;)

--- BBDD/TRANSACCIONES/monousuari.sql
+++ BBDD/TRANSACCIONES/monousuari.sql
@@ -65,6 +65,7 @@ SELECT valor FROM punts WHERE id = 50;
 ```
 
 -- Mostrará 4 
+
 6. Analitzant les següents sentències explica quins canvis es realitzen i on es realitzen. Finalment digues quin valor s'obtindrà amb l'últim SELECT.
 
 ```
