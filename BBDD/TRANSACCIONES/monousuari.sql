ff84422695b5577785f85447cfc8220e2f00e8bf
root-THC <riicardomoreenoi@gmail.com>
2025-05-21 12:39:34 +0200

    root-THC push my project ;)

--- BBDD/TRANSACCIONES/monousuari.sql
+++ BBDD/TRANSACCIONES/monousuari.sql
@@ -52,7 +52,7 @@ SELECT COUNT(*) FROM punts;
 ```
 -- La transacción no ha acabado y como no lo hemos guardado con ningún commmit 
 -- mostrará un SELECT del estado actual dentro de la transacción por lo tanto 
--- 1 fila porque es un count cuyos valores són 40,4
+-- 1 fila porque es un count cuyos valores són (40,4
 
 5. Analitzant les següents sentències explica quins canvis es realitzen i on es realitzen. Finalment digues quin valor s'obtindrà amb l'últim SELECT.
 
