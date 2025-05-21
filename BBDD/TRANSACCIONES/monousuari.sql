b9b0f6ad5f3e053d7afd9971012a76c613d10f58
root-THC <riicardomoreenoi@gmail.com>
2025-05-21 12:48:16 +0200

    root-THC push my project ;)

--- BBDD/TRANSACCIONES/monousuari.sql
+++ BBDD/TRANSACCIONES/monousuari.sql
@@ -23,7 +23,7 @@ UPDATE punts SET valor = 4 WHERE id = 20;
 COMMIT;
 SELECT valor FROM punts WHERE id = 20;
 ```
---Valor será 4 porque con el commit guardamos los cambios q
+--Valor será 4 porque con el commit guardamos los cambios que incluyen un update 
 
 3. Analitzant les següents sentències explica quins canvis es realitzen i on es realitzen. Finalment digues quin valor s'obtindrà amb l'últim SELECT.
 ```
