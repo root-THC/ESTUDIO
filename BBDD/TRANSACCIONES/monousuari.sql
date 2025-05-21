b2a6addff4fc6ba9deb0b74546ae9681d57204e3
root-THC <riicardomoreenoi@gmail.com>
2025-05-21 12:43:53 +0200

    root-THC push my project ;)

--- BBDD/TRANSACCIONES/monousuari.sql
+++ BBDD/TRANSACCIONES/monousuari.sql
@@ -66,7 +66,6 @@ SELECT valor FROM punts WHERE id = 50;
 
 -- Mostraria 5 ya que el select esta mal hecho falta el from y por tanto daria error
 -- al no haber ninguín savepoint volveria al estado de antes de la transacción
---
 
 6. Analitzant les següents sentències explica quins canvis es realitzen i on es realitzen. Finalment digues quin valor s'obtindrà amb l'últim SELECT.
 
