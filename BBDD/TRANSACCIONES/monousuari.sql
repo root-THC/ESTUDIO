3c59b70b9bafdbf324b5b05d63baeb2615c25789
root-THC <riicardomoreenoi@gmail.com>
2025-05-21 12:36:48 +0200

    root-THC push my project ;)

--- BBDD/TRANSACCIONES/monousuari.sql
+++ BBDD/TRANSACCIONES/monousuari.sql
@@ -50,7 +50,7 @@ INSERT INTO punts (id, valor) VALUES (41,7);
 ROLLBACK TO a;
 SELECT COUNT(*) FROM punts;
 ```
--- 
+-- La transacción no ha acabado y como no lo hemos guardado con ningún commmit 
 
 5. Analitzant les següents sentències explica quins canvis es realitzen i on es realitzen. Finalment digues quin valor s'obtindrà amb l'últim SELECT.
 
